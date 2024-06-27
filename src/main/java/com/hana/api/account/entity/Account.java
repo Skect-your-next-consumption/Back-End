9fbfa97cdecf076423a107622b6b2599d1ab60da
TakGyun <gyun1712@gmail.com>
2024-06-27 10:16:39 +0900

    feat: 테이블명 지정

--- src/main/java/com/hana/api/account/entity/Account.java
+++ src/main/java/com/hana/api/account/entity/Account.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
+@Table(name = "accounts")
 @Data
 @Builder
 @NoArgsConstructor
--- src/main/java/com/hana/api/account/entity/Card.java
+++ src/main/java/com/hana/api/account/entity/Card.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "cards")
 @Data
 @Builder
 @NoArgsConstructor
